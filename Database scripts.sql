f5e56d1414423f76f37ece1da156337a2bab0038
NicSwin <9512438@student.swin.edu.au>
2016-05-18 11:12:22 +1000

    improve the database creation script

--- Database scripts.sql
+++ Database scripts.sql
@@ -7,6 +7,9 @@ DATABASE "php-srs"
 NOTE: These can be changed in command line arguments
 */
 
+CREATE SCHEMA `php-srs`;
+USING SCHEMA `php-srs`;
+
 CREATE TABLE `products` (
   `ProductID` int(11) NOT NULL AUTO_INCREMENT,
   `Name` varchar(45) DEFAULT NULL,
@@ -21,6 +24,28 @@ CREATE TABLE `individual_sale` (
   `Quanity` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`SaleID`,`ProductID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8
+
+CREATE TABLE `sales_record` (
+  `SalesRecordID` int(11) NOT NULL AUTO_INCREMENT,
+  `DateTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `SalePrice` decimal(8,2) DEFAULT NULL,
+  PRIMARY KEY (`SalesRecordID`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8
+
+CREATE TABLE `products` (
+  `ProductID` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(45) DEFAULT NULL,
+  `Price` decimal(8,2) DEFAULT NULL,
+  `Stock` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ProductID`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COMMENT='List of all products that can be sold';
+
+CREATE TABLE `individual_sale` (
+  `SaleID` int(11) NOT NULL,
+  `ProductID` int(11) NOT NULL,
+  `Quanity` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`SaleID`,`ProductID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `sales_record` (
   `SalesRecordID` int(11) NOT NULL AUTO_INCREMENT,
